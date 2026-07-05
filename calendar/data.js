fd4a86c3337230ca754d3c8f847114276fbd322c
margaritavelcheva-commits <margarita.velcheva@gmail.com>
2026-07-05 16:47:54 +0300

    Create data.js

---
+++ calendar/data.js
@@ -0,0 +1,141 @@
+const calendarData = {
+
+    month: "ИЮЛЬ",
+
+    days: [
+
+        {
+
+            day: 1,
+
+            weekday: "ПОНЕДЕЛЬНИК",
+
+            meals: [
+
+                {
+
+                    type: "ЗАВТРАК",
+
+                    image: "images/day01_breakfast.jpg",
+
+                    title: "",
+
+                    products: [],
+
+                    recipe: "",
+
+                    balance: {
+
+                        protein: 3,
+
+                        carbs: 3,
+
+                        fat: 3
+
+                    }
+
+                },
+
+                {
+
+                    type: "ПЕРЕКУС",
+
+                    image: "images/day01_snack1.jpg",
+
+                    title: "",
+
+                    products: [],
+
+                    recipe: "",
+
+                    balance: {
+
+                        protein: 1,
+
+                        carbs: 1,
+
+                        fat: 1
+
+                    }
+
+                },
+
+                {
+
+                    type: "ОБЕД",
+
+                    image: "images/day01_lunch.jpg",
+
+                    title: "",
+
+                    products: [],
+
+                    recipe: "",
+
+                    balance: {
+
+                        protein: 3,
+
+                        carbs: 3,
+
+                        fat: 3
+
+                    }
+
+                },
+
+                {
+
+                    type: "ПЕРЕКУС",
+
+                    image: "images/day01_snack2.jpg",
+
+                    title: "",
+
+                    products: [],
+
+                    recipe: "",
+
+                    balance: {
+
+                        protein: 1,
+
+                        carbs: 1,
+
+                        fat: 1
+
+                    }
+
+                },
+
+                {
+
+                    type: "УЖИН",
+
+                    image: "images/day01_dinner.jpg",
+
+                    title: "",
+
+                    products: [],
+
+                    recipe: "",
+
+                    balance: {
+
+                        protein: 3,
+
+                        carbs: 3,
+
+                        fat: 3
+
+                    }
+
+                }
+
+            ]
+
+        }
+
+    ]
+
+};
