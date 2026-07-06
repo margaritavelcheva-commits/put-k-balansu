5361c46d760ff783ce474c24d04da2464d9af60c
margaritavelcheva-commits <margarita.velcheva@gmail.com>
2026-07-06 14:04:41 +0300

    Update app.js

--- calendar/app.js
+++ calendar/app.js
@@ -13,7 +13,9 @@ function renderDay(index){
 
     const day = calendarData.days[index];
 
-    month.textContent = calendarData.month;
+    // Премахнато е показването на месеца
+    month.textContent = "";
+
     dayNumber.textContent = day.day;
     weekday.textContent = day.weekday;
 
@@ -67,28 +69,24 @@ function renderDay(index){
 
 }
 
-document.getElementById("nextDay").onclick=function(){
+document.getElementById("nextDay").onclick = function(){
 
     currentDay++;
 
-    if(currentDay>=calendarData.days.length){
-
-        currentDay=0;
-
+    if(currentDay >= calendarData.days.length){
+        currentDay = 0;
     }
 
     renderDay(currentDay);
 
 }
 
-document.getElementById("prevDay").onclick=function(){
+document.getElementById("prevDay").onclick = function(){
 
     currentDay--;
 
-    if(currentDay<0){
-
-        currentDay=calendarData.days.length-1;
-
+    if(currentDay < 0){
+        currentDay = calendarData.days.length - 1;
     }
 
     renderDay(currentDay);
