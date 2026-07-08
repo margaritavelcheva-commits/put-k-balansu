fbd518b156c93521ff80486ee865a20392503566
margaritavelcheva-commits <margarita.velcheva@gmail.com>
2026-07-08 14:30:50 +0300

    Update data.js

--- calendar/data.js
+++ calendar/data.js
@@ -1,149 +1,75 @@
 const calendarData = {
-
     month: "ИЮЛЬ",
-
     days: [
-
         {
-
             day: 1,
-
             weekday: "ПОНЕДЕЛЬНИК",
-
             meals: [
-
                 {
-
                     type: "ЗАВТРАК",
-
-                    image: "images/day01_breakfast.jpg",
-
+                    image: "images/day01_collage.png",
                     title: "Ветчина из индейки с овсяной лепешкой",
-
                     products: [
-
                         "Ветчина из индейки — 90 г",
-
                         "Овсяная лепешка — 57 г",
-
                         "Миндаль — 10 г"
-
                     ],
-
                     recipe: "Подайте ветчину с овсяной лепешкой. Миндаль используйте как источник полезных жиров.",
-
                     balance: {
-
                         protein: 3,
-
                         carbs: 3,
-
                         fat: 3
-
                     }
-
                 },
-
                 {
-
                     type: "ПЕРЕКУС",
-
                     image: "images/day01_snack1.jpg",
-
                     title: "",
-
                     products: [],
-
                     recipe: "",
-
                     balance: {
-
                         protein: 1,
-
                         carbs: 1,
-
                         fat: 1
-
                     }
-
                 },
-
                 {
-
                     type: "ОБЕД",
-
                     image: "images/day01_lunch.jpg",
-
                     title: "",
-
                     products: [],
-
                     recipe: "",
-
                     balance: {
-
                         protein: 3,
-
                         carbs: 3,
-
                         fat: 3
-
                     }
-
                 },
-
                 {
-
                     type: "ПЕРЕКУС",
-
                     image: "images/day01_snack2.jpg",
-
                     title: "",
-
                     products: [],
-
                     recipe: "",
-
                     balance: {
-
                         protein: 1,
-
                         carbs: 1,
-
                         fat: 1
-
                     }
-
                 },
-
                 {
-
                     type: "УЖИН",
-
                     image: "images/day01_dinner.jpg",
-
                     title: "",
-
                     products: [],
-
                     recipe: "",
-
                     balance: {
-
                         protein: 3,
-
                         carbs: 3,
-
                         fat: 3
-
                     }
-
                 }
-
             ]
-
         }
-
     ]
-
 };
