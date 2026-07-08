c0575202c127f3e1bd8712140bdd5f9e5594fcc5
margaritavelcheva-commits <margarita.velcheva@gmail.com>
2026-07-08 19:27:48 +0300

    Update data.js

--- calendar/data.js
+++ calendar/data.js
@@ -2,139 +2,66 @@ const calendarData = {
     month: "ИЮЛЬ",
     days: [
         {
+            day: "",
+            weekday: "",
+            meals: [
+                {
+                    type: "",
+                    image: "images/day00.png",
+                    title: "",
+                    products: [],
+                    recipe: "",
+                    balance: { protein: 0, carbs: 0, fat: 0 }
+                }
+            ]
+        },
+        {
             day: 1,
             weekday: "ПОНЕДЕЛЬНИК",
             meals: [
-                {
-                    type: "ЗАВТРАК",
-                    image: "images/day01_collage.png",
-                    title: "Ветчина из индейки с овсяной лепешкой",
-                    products: [
-                        "Ветчина из индейки — 90 г",
-                        "Овсяная лепешка — 57 г",
-                        "Миндаль — 10 г"
-                    ],
-                    recipe: "Подайте ветчину с овсяной лепешкой. Миндаль используйте как источник полезных жиров.",
-                    balance: {
-                        protein: 3,
-                        carbs: 3,
-                        fat: 3
-                    }
-                },
-                {
-                    type: "ПЕРЕКУС",
-                    image: "images/day01_snack1.jpg",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 1,
-                        carbs: 1,
-                        fat: 1
-                    }
-                },
-                {
-                    type: "ОБЕД",
-                    image: "images/day01_lunch.jpg",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 3,
-                        carbs: 3,
-                        fat: 3
-                    }
-                },
-                {
-                    type: "ПЕРЕКУС",
-                    image: "images/day01_snack2.jpg",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 1,
-                        carbs: 1,
-                        fat: 1
-                    }
-                },
-                {
-                    type: "УЖИН",
-                    image: "images/day01_dinner.jpg",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 3,
-                        carbs: 3,
-                        fat: 3
-                    }
-                }
+                { type: "ЗАВТРАК", image: "images/day01_collage.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
             ]
         },
         {
             day: 2,
             weekday: "ВТОРНИК",
             meals: [
-                {
-                    type: "ЗАВТРАК",
-                    image: "images/day02.png",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 3,
-                        carbs: 3,
-                        fat: 3
-                    }
-                },
-                {
-                    type: "ПЕРЕКУС",
-                    image: "images/day02_snack1.jpg",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 1,
-                        carbs: 1,
-                        fat: 1
-                    }
-                },
-                {
-                    type: "ОБЕД",
-                    image: "images/day02_lunch.jpg",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 3,
-                        carbs: 3,
-                        fat: 3
-                    }
-                },
-                {
-                    type: "ПЕРЕКУС",
-                    image: "images/day02_snack2.jpg",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 1,
-                        carbs: 1,
-                        fat: 1
-                    }
-                },
-                {
-                    type: "УЖИН",
-                    image: "images/day02_dinner.jpg",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: {
-                        protein: 3,
-                        carbs: 3,
-                        fat: 3
-                    }
-                }
+                { type: "ЗАВТРАК", image: "images/day02.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
+            ]
+        },
+        {
+            day: 3,
+            weekday: "СРЕДА",
+            meals: [
+                { type: "ЗАВТРАК", image: "images/day03.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
+            ]
+        },
+        {
+            day: 4,
+            weekday: "ЧЕТВЕРГ",
+            meals: [
+                { type: "ЗАВТРАК", image: "images/day04.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
+            ]
+        },
+        {
+            day: 5,
+            weekday: "ПЯТНИЦА",
+            meals: [
+                { type: "ЗАВТРАК", image: "images/day05.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
+            ]
+        },
+        {
+            day: 6,
+            weekday: "СУББОТА",
+            meals: [
+                { type: "ЗАВТРАК", image: "images/day06.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
+            ]
+        },
+        {
+            day: 7,
+            weekday: "ВОСКРЕСЕНЬЕ",
+            meals: [
+                { type: "ЗАВТРАК", image: "images/day07.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
             ]
         }
     ]
