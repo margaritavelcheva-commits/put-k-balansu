b22f8530b5938054e75f905484c8d1af3b2e8866
margaritavelcheva-commits <margarita.velcheva@gmail.com>
2026-07-10 07:09:15 +0300

    Update data.js

--- calendar/data.js
+++ calendar/data.js
@@ -5,64 +5,36 @@ const calendarData = {
             day: "",
             weekday: "",
             meals: [
-                {
-                    type: "",
-                    image: "images/day00.png",
-                    title: "",
-                    products: [],
-                    recipe: "",
-                    balance: { protein: 0, carbs: 0, fat: 0 }
-                }
+                { type: "", image: "images/day00.png", title: "", products: [], recipe: "", balance: { protein: 0, carbs: 0, fat: 0 } }
             ]
         },
-        {
-            day: 1,
-            weekday: "ПОНЕДЕЛЬНИК",
-            meals: [
-                { type: "ЗАВТРАК", image: "images/day01_collage.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
-            ]
-        },
-        {
-            day: 2,
-            weekday: "ВТОРНИК",
-            meals: [
-                { type: "ЗАВТРАК", image: "images/day02.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
-            ]
-        },
-        {
-            day: 3,
-            weekday: "СРЕДА",
-            meals: [
-                { type: "ЗАВТРАК", image: "images/day03.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
-            ]
-        },
-        {
-            day: 4,
-            weekday: "ЧЕТВЕРГ",
-            meals: [
-                { type: "ЗАВТРАК", image: "images/day04.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
-            ]
-        },
-        {
-            day: 5,
-            weekday: "ПЯТНИЦА",
-            meals: [
-                { type: "ЗАВТРАК", image: "images/day05.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
-            ]
-        },
-        {
-            day: 6,
-            weekday: "СУББОТА",
-            meals: [
-                { type: "ЗАВТРАК", image: "images/day06.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
-            ]
-        },
-        {
-            day: 7,
-            weekday: "ВОСКРЕСЕНЬЕ",
-            meals: [
-                { type: "ЗАВТРАК", image: "images/day07.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } }
-            ]
-        }
+        { day: 1, weekday: "ПОНЕДЕЛЬНИК", meals: [ { type: "ЗАВТРАК", image: "images/day01_collage.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 2, weekday: "ВТОРНИК", meals: [ { type: "ЗАВТРАК", image: "images/day02.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 3, weekday: "СРЕДА", meals: [ { type: "ЗАВТРАК", image: "images/day03.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 4, weekday: "ЧЕТВЕРГ", meals: [ { type: "ЗАВТРАК", image: "images/day04.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 5, weekday: "ПЯТНИЦА", meals: [ { type: "ЗАВТРАК", image: "images/day05.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 6, weekday: "СУББОТА", meals: [ { type: "ЗАВТРАК", image: "images/day06.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 7, weekday: "ВОСКРЕСЕНЬЕ", meals: [ { type: "ЗАВТРАК", image: "images/day07.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 8, weekday: "ПОНЕДЕЛЬНИК", meals: [ { type: "ЗАВТРАК", image: "images/day08.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 9, weekday: "ВТОРНИК", meals: [ { type: "ЗАВТРАК", image: "images/day09.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 10, weekday: "СРЕДА", meals: [ { type: "ЗАВТРАК", image: "images/day10.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 11, weekday: "ЧЕТВЕРГ", meals: [ { type: "ЗАВТРАК", image: "images/day11.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 12, weekday: "ПЯТНИЦА", meals: [ { type: "ЗАВТРАК", image: "images/day12.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 13, weekday: "СУББОТА", meals: [ { type: "ЗАВТРАК", image: "images/day13.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 14, weekday: "ВОСКРЕСЕНЬЕ", meals: [ { type: "ЗАВТРАК", image: "images/day14.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 15, weekday: "ПОНЕДЕЛЬНИК", meals: [ { type: "ЗАВТРАК", image: "images/day15.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 16, weekday: "ВТОРНИК", meals: [ { type: "ЗАВТРАК", image: "images/day16.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 17, weekday: "СРЕДА", meals: [ { type: "ЗАВТРАК", image: "images/day17.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 18, weekday: "ЧЕТВЕРГ", meals: [ { type: "ЗАВТРАК", image: "images/day18.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 19, weekday: "ПЯТНИЦА", meals: [ { type: "ЗАВТРАК", image: "images/day19.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 20, weekday: "СУББОТА", meals: [ { type: "ЗАВТРАК", image: "images/day20.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 21, weekday: "ВОСКРЕСЕНЬЕ", meals: [ { type: "ЗАВТРАК", image: "images/day21.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 22, weekday: "ПОНЕДЕЛЬНИК", meals: [ { type: "ЗАВТРАК", image: "images/day22.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 23, weekday: "ВТОРНИК", meals: [ { type: "ЗАВТРАК", image: "images/day23.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 24, weekday: "СРЕДА", meals: [ { type: "ЗАВТРАК", image: "images/day24.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 25, weekday: "ЧЕТВЕРГ", meals: [ { type: "ЗАВТРАК", image: "images/day25.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 26, weekday: "ПЯТНИЦА", meals: [ { type: "ЗАВТРАК", image: "images/day26.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 27, weekday: "СУББОТА", meals: [ { type: "ЗАВТРАК", image: "images/day27.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] },
+        { day: 28, weekday: "ВОСКРЕСЕНЬЕ", meals: [ { type: "ЗАВТРАК", image: "images/day28.png", title: "", products: [], recipe: "", balance: { protein: 3, carbs: 3, fat: 3 } } ] }
     ]
 };
